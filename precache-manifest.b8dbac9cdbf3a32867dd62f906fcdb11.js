5d67174a2443cef46e47c28104d6db5cd18a33bd
MMMurugan <59476995+MMMurugan@users.noreply.github.com>
2020-01-04 23:45:53 +0530

    Add files via upload

---
+++ precache-manifest.b8dbac9cdbf3a32867dd62f906fcdb11.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec97b76991a5e483e5770e3cdb2249f7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "19dfbd04c8d39fde6758",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "b5027e64737ef3f52f1e",
+    "url": "/static/css/main.87c2df7e.chunk.css"
+  },
+  {
+    "revision": "19dfbd04c8d39fde6758",
+    "url": "/static/js/2.4ff608d4.chunk.js"
+  },
+  {
+    "revision": "b5027e64737ef3f52f1e",
+    "url": "/static/js/main.e7b31957.chunk.js"
+  },
+  {
+    "revision": "5a702f4d5d7287c2007a",
+    "url": "/static/js/runtime-main.9290c216.js"
+  },
+  {
+    "revision": "0913a9df3870df20016ad9420328b4b2",
+    "url": "/static/media/notification.0913a9df.svg"
+  },
+  {
+    "revision": "1d9e6c3ae89c33d3359a46e8226621e7",
+    "url": "/static/media/profitability.1d9e6c3a.svg"
+  },
+  {
+    "revision": "f7d3ed3810ca33100bf365ab8ddf43fb",
+    "url": "/static/media/school-img1.f7d3ed38.jpeg"
+  }
+]);
